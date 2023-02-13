5a7290dcb6c78f5558c0789f1cadceb021f97b43
Kridbhume Chammanard <kfccoolguy@gmail.comlist>
2023-02-13 13:27:46 +0700

    initial commit

--- test.py
+++ test.py
@@ -1,104 +1,22 @@
-import cv2
-from cvzone.HandTrackingModule import HandDetector
-import numpy as np 
-import math
-import time
-from roboflow import Roboflow
+# This requires internet connection to work, if required to work without internet use the other file
+# if error use playsound 1.2.2
+
 import os
-import string
-import pyttsx3
-import keyboard
-from glob import glob
-from ultralytics import YOLO
+import playsound
+from gtts import gTTS
 
-PATH_TO_MODEL = 'yolov8/training_results/sign_language/weights/best.pt'
-model = YOLO(PATH_TO_MODEL)
+SAVE_PATH = "./voutput"
 
-cap = cv2.VideoCapture(0)
-offset = 20
-imgSize = 300
-classes = dict( (i, key) for i,key in enumerate(string.ascii_lowercase))
+class gttsModule:
 
-detector = HandDetector(maxHands=1)
-sentence = []
+    def __init__(self, text, language="en", save_path=SAVE_PATH, accent="com"):
+        self.engine = gTTS(text, tld=accent, lang=language, slow=False)
+        self.path = save_path
+        self.engine.save(f"{self.path}/talk.mp3")
 
-def hands_feed():
-    count = 0
-    letter_count = 0
-    while True:
-        ret,frame = cap.read()
-        hands = detector.findHands(frame, draw=False)
-        #hands, frame = detector.findHands(frame)
-        if hands:
-            hand = hands[0]
-            x,y,w,h = hand['bbox']
+    def speak(self):
+        playsound.playsound(f"{self.path}/talk.mp3")
 
-            imgWhite = np.ones((imgSize,imgSize,3),np.uint8)*255  #uint8 is 0-255
-            imgCrop = frame[y-offset:y+h+offset,x-offset:x+w+offset]
-
-            aspectRatio = h/w
-
-            if aspectRatio > 1:
-                k = imgSize/h
-                width = math.ceil(k*w)
-                imgResize = cv2.resize(imgCrop,(width,imgSize))
-                wGap = math.ceil((imgSize-width)/2)
-                imgWhite[:,wGap:width+wGap] = imgResize
-
-            else:
-                k = imgSize/w
-                height = math.ceil(k*h)
-                imgResize = cv2.resize(imgCrop,(imgSize,height))
-                hGap = math.ceil((imgSize-height)/2)
-                imgWhite[hGap:height+hGap,:] = imgResize
-
-            if keyboard.read_key() == "t":
-                cv2.imwrite(f"imglib/image_{letter_count}.jpg",imgWhite)
-            else:pass
-            
-            count += 1
-            results = model.predict(source=imgWhite, conf=0.3, show=True)[0]
-        
-        else:
-            count = 0
-            
-            #cv2.imshow("Whites", imgWhite)
-
-        #cv2.imshow("frame",frame)
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-def predict_image():
-    folder = "imglib/"
-    images = glob(os.path.join(folder,"*jpg"))
-    for image in images:
-        img = cv2.imread(image)
-        results = model.predict(source = img, conf = 0.5)[0]
-        if results.boxes:
-            for i,obj in enumerate(results.boxes):
-                x1, y1, x2, y2, conf, cls = obj.data.cpu().detach().numpy()[0]
-                letter = classes[int(cls)]
-                sentence.append(letter)
-        else:
-            sentence.append(" ")
-    print("".join(sentence))
-
-def reset_images():
-    folder = "imglib/"
-    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    for file in files:
-        os.remove(os.path.join(folder, file))
-
-def text_to_speech():
-    engine = pyttsx3.init()
-    engine.say("".join(sentence))
-    engine.runAndWait()
-
-if __name__== "__main__":
-    reset_images()
-    hands_feed()
-    predict_image()
-    text_to_speech()
+if __name__ == "__main__":
+    engine = gttsModule(text="tinganinganinganinganinganing", accent="co.in")
+    engine.speak()
